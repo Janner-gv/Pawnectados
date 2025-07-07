159c6b76d7c40e6d99e32bb9961fd9b9064d1dd1
Patricia <Jeimypcortes@gmail.com>
2025-07-06 22:06:47 -0500

    Proyecto editado Patricia

---
+++ src/main/java/com/example/Pawnectados/modelo/animal.java
@@ -0,0 +1,92 @@
+package com.example.Pawnectados.modelo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "animales")
+public class animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_animal")
+    private Long id;
+
+    private String nombre;
+    private String especie;
+    private String raza;
+    private int edad;
+    private String descripcion;
+    private String estado;
+
+    @Column(name = "foto")
+    private String imagenUrl;
+
+    // Constructor vacío (obligatorio para JPA)
+    public animal() {}
+
+    // Getters y Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+}
